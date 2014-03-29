d3b96b1187bf7666f4a6b48e10c2c36f89c05119
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-03-29 23:41:05 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.4'
+
+gem 'mysql2'
+
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+gem 'uglifier', '>= 1.3.0'
+gem 'turbolinks'
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.2'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'slim-rails'
+
+gem 'unicorn'
+
+group :development do
+	gem 'capistrano'
+	gem 'debugger'
+end
+
+# Use debugger
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whitewalls::Application.config.secret_key_base = 'da3e6abdea6b5f32b8c0e59df19c85c3b27c3faa53b23c50f06810deee637b40949148001c935c63303e96bfa344f66dbf3a021e0048a744aad057cb612273cd'
