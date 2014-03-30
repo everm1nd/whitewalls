f4d3db549a13f0468c51a366cd0605b2f59ed4e2
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-03-31 01:07:21 +0400

    added db/seed for fast Artwork seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+titles = ['Sun', 'Moon', 'Storm', 'Scream', 'Howl', 'Life and Death', 'Jupiter', 'Mars', 'Space', 'Tree', 'Face', 'Somebody', 'Lace']
+
+Artist.find_or_create_by_name('Von Goh')
+
+75.times do
+	puts Artwork.create(
+		title: titles.sample,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore',
+		year: rand(1950..2013),
+		price: rand(1..1000000),
+		artist: Artist.last,
+		remote_image_url: 'http://lorempixel.com/500/500/'
+	).errors.full_messages
+end	
