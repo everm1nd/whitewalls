3d17fe50e942a61c096a76b139934b9f1fce7311
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-03-30 21:49:33 +0400

    added Image uploader

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # TODO: add default images and set link to it
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  version :medium do
+    process :resize_to_fit => [470, 380]
+  end
+
+  version :preview do
+    process :resize_to_fit => [305, 260]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
