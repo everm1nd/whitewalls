5c7bce3e21a4985195548a7d7ddcefad4c2b9743
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-03-31 00:26:54 +0400

    added validations for Artist and Artwork

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ActiveRecord::Base
 	has_many :artworks
+
+	validates_presence_of :name
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,8 @@ class Artwork < ActiveRecord::Base
 	belongs_to :artist
 
 	mount_uploader :image, ImageUploader
+
+	paginates_per 14
+
+	validates_presence_of :title, :description, :year, :price, :artist, :image
 end
