15873a57ae40629599d067f222b9aa88781c9042
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-05-06 00:28:15 +0400

    added page number class in Home views

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,6 @@
 class ArtworksController < ApplicationController
   def show
+  	# TODO: refactor this with scopes or any
   	@artwork      = Artwork.find params[:id]
   	@artist_works = @artwork.artist.artworks.where('id != ?', @artwork.id).limit(6)
   	@other_works  = Artwork.where('id NOT IN (?)', @artist_works.ids << @artwork.id).limit(9)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def in_rubbles(price)
 		number_to_currency(price, unit: "руб.", format: "%n %u", precision: 0, delimiter: ' ')
 	end
+
+	def current_page
+		params[:page] || 1
+	end
 end
