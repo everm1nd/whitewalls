c09c247a13753bd05c5a286403ab85f405281a30
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-06-18 01:49:52 +0400

    close buy_popup after send request

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,9 @@ class OrdersController < ApplicationController
   def create
     order = Order.new params[:order]
     order.notify_admin
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js   { }
+    end
   end
 end
---
+++ app/views/orders/create.js.coffee
@@ -0,0 +1,1 @@
+$(".popup-wrapper-buy").css "display", "none"
