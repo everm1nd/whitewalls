b6b21b29baacd0ca564f8c47d998ea2eb23466e2
everm1nd <everm1nd.mail@gmail.com>
2014-06-17 20:38:44 +0400

    cleared #about and #contacts actions form HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,4 @@ class HomeController < ApplicationController
   def index
   	@collections = Collection.all
   end
-
-  def about
-  end
-
-  def contacts
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Whitewalls::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
 
   root 'home#index'
 
-  get 'about', to: 'home#about', as: :about
-
   resources :artists, only: :show
-  
   resources :artworks, only: [:index, :show]
-
   resources :collections, only: :show
 
 end
