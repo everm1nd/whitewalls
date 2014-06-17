772dc52e4f5cdbd81939b8879dd3860c7c3e2d60
Nikita Simakov <nikitasimakov@MacBook-Pro-Nikita.local>
2014-06-18 01:53:38 +0400

    added email settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Whitewalls::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "everm1nd.mail@gmail.com",
+    :password  => "qAHgK4UxFO25PXwC8jVvcQ", # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'google.com', # your domain to identify your server when connecting
+  }
 end
