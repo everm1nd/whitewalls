6c8ae21184ee2a57ab3d55c4e31eeac2a8b5c2a5
everm1nd <everm1nd.mail@gmail.com>
2014-06-23 17:13:29 +0400

    fixed 'translation missing' error

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module Whitewalls
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
     config.i18n.fallbacks      = [:en]
+    config.i18n.locale         = :ru
   end
 end
